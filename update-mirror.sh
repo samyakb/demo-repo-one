a71380bfeec9dcb74f77ffe485be9614c0058c3f
Samyak Bakliwal <99634190+samyakb@users.noreply.github.com>
2022-03-17 13:18:08 +0530

    Update update-mirror.sh

--- update-mirror.sh
+++ update-mirror.sh
@@ -11,8 +11,9 @@ remote_repo="https://${GITHUB_ACTOR}:${BOT_TOKEN}@github.com/${REPOSITORY}"
 echo "${remote_repo}"
 
 # git push "${remote_repo}" HEAD:main --force-with-lease
-git pull upstream main
 git remote add upstream "${remote_repo}"
+
+git pull upstream main
 
 echo "Echo remote"
 git remote -v
