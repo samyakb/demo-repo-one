59599a5fe2af638ba16b241286845e03925258e2
Samyak Bakliwal <samyakb@mtechzilla.com>
2022-03-18 13:02:19 +0530

    update mirror script

--- update-mirror.sh
+++ update-mirror.sh
@@ -14,15 +14,4 @@ echo "${remote_repo}"
 # git push "${remote_repo}" HEAD:main --force-with-lease
 git remote add upstream "${remote_repo}"
 
-# git pull upstream main
-
-echo "Echo remote"
-git remote -v
-
-# remove .github workflow folder
-git rm --cached -r .github
-# git add -f .github
-git commit -m "removed workflow files from tracking"
-ls -la
-git status
 git push -u upstream main
